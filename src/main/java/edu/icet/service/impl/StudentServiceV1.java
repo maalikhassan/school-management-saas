696b38edfb538f395f2603aa00ff5ce861ced790
Maalik_Hassan <maalikhassan1@gmail.com>
2026-01-15 17:12:58 +0530

    fixed the spaces in the yml file. (jpa inline with url,hibernate inline with url),
    *mapped the controller to the implemetation
    
    *executed the repository mthods into the impl

--- src/main/java/edu/icet/service/impl/StudentServiceV1.java
+++ src/main/java/edu/icet/service/impl/StudentServiceV1.java
@@ -6,11 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentServiceV1 { // removed 'implements StudentService'
 
-
     public String getVersion() {
         return "v1.0.0";
     }
-
 
     public String getName() {
         return "Student Xyz";
