2fb7a083203da2249eec0c86ac40278f13e1f40a
Maalik_Hassan <maalikhassan1@gmail.com>
2026-01-15 12:21:37 +0530

    tried something new, made a defual get land in a homepage html, added spring thyme tependency, added html in resources, and mapped fault in seperate controller

---
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -0,0 +1,13 @@
+package edu.icet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/get-student")
+    public String getStudentName(){
+        return "Student";
+    }
+}
